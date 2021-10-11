94b05ee4ebdd8b93a6eb2475d3a4994bf4b69927
767798017@qq.com <767798017@qq.com>
2021-10-11 22:32:08 +0800

    jpa

---
+++ jpa/src/main/java/com/heng/mall/model/Admin.java
@@ -0,0 +1,122 @@
+package com.heng.mall.model;
+
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name="ums_admin")
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="id")
+    private Integer id;
+
+    @Column(name="username")
+    private String username;
+
+    @Column(name="name")
+    private String password;
+
+    @Column(name="nickName")
+    private String nickName;
+
+    @Column(name="phone")
+    private String phone;
+
+    @Column(name="email")
+    private String email;
+
+    @Column(name="note")
+    private String note;
+
+    @Column(name="createTime")
+    private Date createTime;
+
+    @Column(name="loginTime")
+    private Date loginTime;
+
+    @Column(name="status")
+    private Integer status;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getLoginTime() {
+        return loginTime;
+    }
+
+    public void setLoginTime(Date loginTime) {
+        this.loginTime = loginTime;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+}
