afd875fbc4a5b83fa9d5c63bf06ae68bd92eb8d0
767798017@qq.com <767798017@qq.com>
2021-10-17 11:24:17 +0800

    优惠卷管理

---
+++ wm-jpa/src/main/java/com/meituan/waimai/po/CouponGoodsRelation.java
@@ -0,0 +1,53 @@
+package com.meituan.waimai.po;
+
+
+import lombok.Data;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name="sms_coupon_goods_relation")
+public class CouponGoodsRelation {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name="id")
+    private Integer id;
+
+    @Column(name="id")
+    private Integer couponId;
+
+    @Column(name="shop_id")
+    private Integer shopId;
+
+    @Column(name="shop_id")
+    private Integer goods_Id;
+
+    @Column(name="goods_name")
+    private String goodsName;
+
+    @Column(name="goods_sn")
+    private String goodsSn;
+
+    @CreatedDate
+    @Column(name = "created_date", updatable = false)
+    private Date createdDate;
+
+    @CreatedBy
+    @Column(name = "created_by", updatable = false, length = 64)
+    private String createdBy;
+
+    @LastModifiedDate
+    @Column(name = "updated_date")
+    private Date updatedDate;
+
+    @LastModifiedBy
+    @Column(name = "updated_by", length = 64)
+    private String updatedBy;
+}
