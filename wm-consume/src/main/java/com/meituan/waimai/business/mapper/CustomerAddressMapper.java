2eceaced29768caf551eddf66cba5e9d5f9490bb
wenheng <wenheng4034186@163.com>
2021-12-31 17:34:28 +0800

    mapper

---
+++ wm-consume/src/main/java/com/meituan/waimai/business/mapper/CustomerAddressMapper.java
@@ -0,0 +1,7 @@
+package com.meituan.waimai.business.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.meituan.waimai.business.model.entity.CustomerAddress;
+
+public interface CustomerAddressMapper extends BaseMapper<CustomerAddress> {
+}
---
+++ wm-consume/src/main/java/com/meituan/waimai/business/model/entity/CustomerAddress.java
@@ -0,0 +1,59 @@
+package com.meituan.waimai.business.model.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import java.util.Date;
+import lombok.Data;
+
+@ApiModel(value="com-meituan-waimai-business-model-entity-CustomerAddress")
+@Data
+@TableName(value = "cms_customer_address")
+public class CustomerAddress {
+    @TableId(value = "id", type = IdType.INPUT)
+    @ApiModelProperty(value="")
+    private Integer id;
+
+    @TableField(value = "customer_id")
+    @ApiModelProperty(value="")
+    private Integer customerId;
+
+    @TableField(value = "gender")
+    @ApiModelProperty(value="")
+    private Integer gender;
+
+    @TableField(value = "house_number")
+    @ApiModelProperty(value="")
+    private String houseNumber;
+
+    @TableField(value = "`name`")
+    @ApiModelProperty(value="")
+    private String name;
+
+    @TableField(value = "phone")
+    @ApiModelProperty(value="")
+    private String phone;
+
+    @TableField(value = "poi")
+    @ApiModelProperty(value="")
+    private String poi;
+
+    @TableField(value = "created_by")
+    @ApiModelProperty(value="")
+    private String createdBy;
+
+    @TableField(value = "created_date")
+    @ApiModelProperty(value="")
+    private Date createdDate;
+
+    @TableField(value = "updated_by")
+    @ApiModelProperty(value="")
+    private String updatedBy;
+
+    @TableField(value = "updated_date")
+    @ApiModelProperty(value="")
+    private Date updatedDate;
+}
